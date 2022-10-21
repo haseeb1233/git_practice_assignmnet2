ca4f8463e067198d735f5aa64f4efa215635fd59
mdharoonhussain <haroonhussain97@gmail.com>
2022-10-21 22:37:01 +0530

    correct_palindrome_problem

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,12 @@
  let str="aba"
 let bag=""
 
- for(i=0;i<str.length;i++){
   for(j=str.length-1;j>=0;j--){
-    bag+=str[str.length-1]
+    bag+=str[j]
    }
-}
 if(str==bag){
- console.log("Yes)}
-else{console.log("no")
+ console.log("Yes")
+ }
+else{
+  console.log("no")
+}
